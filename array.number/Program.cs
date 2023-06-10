74b31046703bfb929f654bb3ccb93b7ea5bdd5be
hp <hp@DESKTOP-2VE9LHI>
2023-06-10 13:21:07 +0400

    Add project files.

---
+++ array.number/Program.cs
@@ -0,0 +1,64 @@
+﻿
+//* მთელ რიცხვთა მასივი, კონსოლშ გამოგვაქვს ლუწი რიცხვები*// +
+//*მთელ რიცხვთა მასივი, კონსოლში გამოაქვს ყველა რიცხვის ჯამი*// +
+//*მთელ რიცხვთა მასივი, კონსოლში გამოაქვს ყველა რიცხვის ნამრავლი *//+
+//*დაითვალეთ პირველი  n ნატურალური რიცხვის ჯამი *//
+//*კონსოლში შეიყვანეთ შემთხვევითი რიცხვი და დააბრუნოს ამ რიცხვის კუბი *//
+
+
+using System.Diagnostics.CodeAnalysis;
+
+//int[] numbers = new int[] {6,12,7,11,21,9,22,10};
+//int sum = 0;    
+//foreach (var i  in numbers)
+//  sum += i;
+//Console.WriteLine(sum);
+//Console.ReadLine(); 
+
+
+
+//int[] numbers = new int[] { 6,2,1,3,1,10 };
+//int sum = 1;
+//foreach (var i in numbers)
+//    sum *= i;
+//Console.WriteLine(sum);
+//Console.ReadLine();
+
+//
+//int[] numbers = new int[] {1, 3, 6, 10, 12};
+//int evencount = 0, oddcount = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+// if (numbers[i] % 2 == 0)
+//  {
+//      Console.WriteLine(numbers[i] + "  even number");
+//      evencount++;
+//  }
+//  else
+//  {
+//      Console.WriteLine(numbers[i] + "  odd number");
+//      evencount++;
+//  }
+
+//}    
+
+
+//Console.WriteLine("cobe of number");
+//while (true) 
+//{
+//    Console.WriteLine("enter the number :\t");
+
+ //   int num = Convert.ToInt32(Console.ReadLine());
+
+//    int result = num * num * num;
+//    Console.WriteLine("cube of number" + num + "is : " + result);
+
+//}
+//Console.ReadLine();
+
+
+
+
+
+
+
